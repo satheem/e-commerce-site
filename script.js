088546c19294a620c168b76e59f13362a4c2d06a
satheem <satheem511@gmail.com>
2025-03-12 09:18:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function addToCart() {
+    alert('Added to Cart!');
+}
+
+// JavaScript for toggling the menu visibility on mobile
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('active');
+});
+
+
+function registerUser() {
+    // You can validate form fields here if needed
+    alert("User registered successfully!");
+}
+
+function loginUser() {
+    // You can add form validation here if needed
+    alert("Login successful!");
+}
